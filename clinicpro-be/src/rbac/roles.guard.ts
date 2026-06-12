f1c2c4c57662627101edbb0e92c96dab9565c5d4
doqhuy268 <doqhuy268@gmail.com>
2026-06-12 16:00:00 +0700

    feat: login email/password + jwt 15d

---
+++ clinicpro-be/src/rbac/roles.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from './public.decorator';
+import { ROLES_KEY } from './roles.decorator';
+import { Role } from './roles.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) return true;
+
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) return true;
+
+    const request = context.switchToHttp().getRequest<{
+      user?: { role?: Role };
+      headers: Record<string, string | undefined>;
+    }>();
+    const { user } = request;
+
+
+    return requiredRoles.includes(user?.role as Role);
+  }
+}
